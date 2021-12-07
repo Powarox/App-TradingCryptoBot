469bb767d5b6c13f45e64f12b9aa86d3d579bf95
Powarox219 <moriniere.robin1877@gmail.com>
2021-12-07 22:26:13 +0100

    add index fear and greed

--- Python/script.py
+++ Python/script.py
@@ -1,21 +1,36 @@
 # https://github.com/man-c/pycoingecko
+import fear_and_greed
 from pycoingecko import CoinGeckoAPI
-
 
 # Instanciation Api
 coinApi = CoinGeckoAPI()
-
 
 # Mes liste de coin
 listCoin = ['bitcoin', 'ethereum', 'polkadot', 'avalanche-2', 'terra-luna']
 listBlockchain = ['binancecoin', 'ripple', 'solana', 'matic-network', 'cosmos']
 listCurrencies = ['usd', 'eur']
 
-
 # Get listCoin price & marketcap
+print(' --- Coin Price --- ')
 result = coinApi.get_price(ids = listCoin, vs_currencies = 'usd', include_market_cap = 'true')
-print(result)
+
+# Affichage
+for coin, price in result.items():
+    print(coin + ' : ' + str(price['usd']) + '$')
 
 
 
-# Get listCoin
+
+# Fear And Greed Index
+indexFAG = fear_and_greed.get()
+print(' \n' + ' --- Fear And Greed Index --- ')
+print(indexFAG)
+
+
+
+
+
+
+
+
+#
