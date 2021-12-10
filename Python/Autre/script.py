6b877b91b2cc3e4f9822107a59c897b405b0599e
Powarox219 <moriniere.robin1877@gmail.com>
2021-12-10 18:21:15 +0100

    Order Folder

--- Python/Autre/script.py
+++ Python/Autre/script.py
@@ -1,9 +1,11 @@
 # https://github.com/man-c/pycoingecko
 import fear_and_greed
 from pycoingecko import CoinGeckoAPI
+# from Portfolio import Portfolio
 
-# import Portfolio
 
+# walletAvax = Portfolio('Wallet_AVAX', 'avalanche-2', 'AVAX', 100, 'usd')
+# print(walletAvax.getWallet())
 
 # Instanciation Api
 coinApi = CoinGeckoAPI()
@@ -11,6 +13,7 @@ coinApi = CoinGeckoAPI()
 # Mes liste de coin
 listCoin = ['bitcoin', 'ethereum', 'polkadot', 'avalanche-2', 'terra-luna']
 listBlockchain = ['binancecoin', 'ripple', 'solana', 'matic-network', 'cosmos']
+listStableCoin = ['usd-coin', 'tether', 'terrausd']
 listCurrencies = ['usd', 'eur']
 
 # Get listCoin price & marketcap
@@ -22,14 +25,16 @@ for coin, price in result.items():
     print(coin + ' : ' + str(price['usd']) + '$')
 
 
-
-
 # Fear And Greed Index
 indexFAG = fear_and_greed.get()
 print(' \n' + ' --- Fear And Greed Index --- ')
 print(indexFAG[1] + ' : ' + str(indexFAG[0]))
 print('date : ' + str(indexFAG[2]))
 print(indexFAG)
+
+
+
+
 
 
 
